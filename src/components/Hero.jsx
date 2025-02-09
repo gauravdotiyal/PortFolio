65ad36fb64da644bc887fd936ab099495ea7784c
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-02-09 19:36:09 +0530

    Profile Intro Section Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { HERO_CONTENT } from '../constants'
+import profilePic from '../assets/kevinRushProfile.jpg'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='border-b border-neutral-900 lg:mb-36'>
+       <div className='flex flex-wrap'>
+         <div className='w-full lg:w-1/2'>
+             <div className="flex flex-col items-center lg:items-start">
+                <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
+                Gaurav Dotiyal
+                </h1>
+                <span className='bg-gradient-to-r from-pink-300 via-slate-200 to-purple-300 bg-clip-text text-4xl tracking-tight text-transparent'>Full Stack Developer</span>
+                <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>
+                    {HERO_CONTENT}
+                </p>
+             </div>
+         </div>
+          <div className="w-full lg:w-1/2 lg:p-8">
+             <div className="flex justify-center">
+                <img src={profilePic} alt="GauravDotiyal" />
+             </div>
+          </div>
+       </div>
     </div>
   )
 }
